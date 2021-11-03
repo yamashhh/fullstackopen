09a1e65471f3e409d724870775aad4aa5a06e5c8
Shogo Yamato <yamash.yamash@gmail.com>
2021-11-03 19:33:12 +0900

    feat: finished part2-d demos

--- part2/part2-notes/src/App.js
+++ part2/part2-notes/src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
-import axios from "axios";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -10,9 +10,16 @@ const App = () => {
   useEffect(() => {
     console.log("effect");
     const fetchData = async () => {
+      // setNotes(await noteService.getAll());
+      setNotes(
+        (await noteService.getAll()).concat({
+          id: 10000,
+          content: "This note is not saved to server",
+          date: "2019-05-30T17:30:31.098Z",
+          important: true,
+        })
+      );
       console.log("promise fulfilled");
-      const { data } = await axios.get("http://localhost:3001/notes");
-      setNotes(data);
     };
     fetchData();
   }, []);
@@ -21,18 +28,35 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
-  const addNote = (event) => {
+  const addNote = async (event) => {
     event.preventDefault();
-    setNotes((prev) => [
-      ...prev,
-      {
-        content: newNote,
-        date: new Date().toISOString(),
-        important: Math.random() < 0.5,
-        id: prev.length + 1,
-      },
-    ]);
+    const returnedNote = await noteService.create({
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    });
+    setNotes((prev) => [...prev, returnedNote]);
     setNewNote("");
+  };
+
+  const toggleImportanceOf = async (id) => {
+    const note = notes.find((n) => n.id === id);
+    try {
+      const returnedNote = await noteService.update(id, {
+        ...note,
+        important: !note.important,
+      });
+      setNotes((prev) =>
+        prev.map((note) => (note.id === id ? returnedNote : note))
+      );
+    } catch (error) {
+      error.request.status === 404 || error.response.status === 404
+        ? alert(
+            `the note '${note.content}' was already deleted from the server`
+          )
+        : console.error({ ...error });
+      setNotes((prev) => prev.filter((n) => n.id !== id));
+    }
   };
 
   return (
@@ -43,7 +67,11 @@ const App = () => {
       </button>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
---
+++ part2/part2-notes/src/services/notes.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/notes";
+
+const getAll = async () => {
+  return (await axios.get(baseUrl)).data;
+};
+
+const create = async (newObject) => {
+  return (await axios.post(baseUrl, newObject)).data;
+};
+
+const update = async (id, newObject) => {
+  return (await axios.put(`${baseUrl}/${id}`, newObject)).data;
+};
+
+export default {
+  getAll,
+  create,
+  update,
+};
