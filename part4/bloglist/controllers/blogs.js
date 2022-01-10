40d0c3e365b9cd9917a019eac73cd3a3309d1ea7
Shogo Yamato <yamash.yamash@gmail.com>
2022-01-10 19:23:03 +0900

    feat: exercies -4.2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import Blog from '../models/blog.js'
+
+const blogsRouter = Router()
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+export default blogsRouter
---
+++ part4/notes/controllers/notes.js
@@ -0,0 +1,61 @@
+import Note from "../models/note.js";
+import express from "express";
+
+const notesRouter = express.Router();
+
+notesRouter.get("/", async (_, response) => {
+  const notes = await Note.find();
+  response.json(notes);
+});
+
+notesRouter.get("/:id", async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id);
+    note ? response.json(note) : response.status(404).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+notesRouter.post("/", async (request, response, next) => {
+  const { content, important = false } = request.body;
+
+  const note = new Note({
+    content,
+    important,
+    date: new Date(),
+  });
+
+  try {
+    const savedNote = await note.save();
+    response.json(savedNote);
+  } catch (error) {
+    next(error);
+  }
+});
+
+notesRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Note.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+notesRouter.put("/:id", async (request, response, next) => {
+  const { content, important } = request.body;
+
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      { new: true }
+    );
+    response.json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default notesRouter;
