03ed9d02385368e8e42fed43569a4a286ad9beb2
Shogo Yamato <yamash.yamash@gmail.com>
2023-02-26 22:08:00 +0900

    feat: part 8 subscriptions

--- part8/phonebook/backend/src/index.ts
+++ part8/phonebook/backend/src/index.ts
@@ -1,24 +1,27 @@
-import {
-  ApolloServer,
-  AuthenticationError,
-  UserInputError,
-} from "apollo-server";
+import { ApolloServer } from "@apollo/server";
 import { readFileSync } from "fs";
 import path from "path";
-import { Resolvers, YesNo } from "./generated/graphql";
 import mongoose, { Error, Types } from "mongoose";
-import Person, { PersonType } from "./models/person";
 import jwt from "jsonwebtoken";
 import User from "./models/user";
-
+import resolvers from "./resolvers";
+import express from "express";
+import http from "http";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import cors from "cors";
+import { expressMiddleware } from "@apollo/server/express4";
+import { WebSocketServer } from "ws";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { useServer } from "graphql-ws/lib/use/ws";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
 const MONGODB_URI = process.env["MONGODB_URI"];
 if (!MONGODB_URI) {
   throw new Error("Failed to read MONGODB_URI.");
 }
+export const JWT_SECRET = process.env["JWT_SECRET"];
 
-(async (): Promise<void> => {
+(async () => {
   try {
     console.log("connecting to", MONGODB_URI);
     await mongoose.connect(MONGODB_URI);
@@ -34,138 +37,55 @@ const typeDefs = readFileSync(
   "utf8"
 );
 
-const JWT_SECRET = process.env["JWT_SECRET"];
-
-const resolvers: Resolvers = {
-  Query: {
-    async personCount() {
-      return Person.collection.countDocuments();
-    },
-    async allPersons(_, args) {
-      if (!args.phone) {
-        return Person.find({});
-      }
-      return Person.find({ phone: { $exists: args.phone === YesNo.Yes } });
-    },
-    async findPerson(_, args) {
-      return Person.findOne({ name: args.name });
-    },
-    me(_, __, context) {
-      return context.currentUser;
-    },
-  },
-  Person: {
-    address: (root) => {
-      return {
-        street: root.street,
-        city: root.city,
-      };
-    },
-  },
-  Mutation: {
-    async addPerson(_, args, context) {
-      const person = new Person({ ...args });
-      const currentUser = context.currentUser;
-      if (!currentUser) {
-        throw new AuthenticationError("not authenticated");
-      }
-
-      try {
-        const result = await person.save();
-        currentUser.friends = currentUser.friends.concat(result);
-        await currentUser.save();
-        return result;
-      } catch (error) {
-        throw new UserInputError((error as Error).message, {
-          invalidArgs: args,
-        });
-      }
-    },
-    async editNumber(_, args) {
-      const person = await Person.findOne({ name: args.name });
-
-      if (!person) {
-        return null;
-      }
-
-      person.phone = args.phone;
-
-      try {
-        const result = await person.save();
-        return result;
-      } catch (error) {
-        throw new UserInputError((error as Error).message, {
-          invalidArgs: args,
-        });
-      }
-    },
-    // @ts-ignore
-    async createUser(_, args) {
-      const user = new User({ username: args.username });
-
-      try {
-        const result = await user.save();
-        return result;
-      } catch (error) {
-        throw new UserInputError((error as Error).message, {
-          invalidArgs: args,
-        });
-      }
-    },
-    async login(_, args) {
-      const user = await User.findOne({ username: args.username });
-
-      if (!user || args.password !== "secret") {
-        throw new UserInputError("wrong credentials");
-      }
-
-      const userForToken = {
-        username: user.username,
-        id: user._id,
-      };
-
-      return { value: jwt.sign(userForToken, JWT_SECRET ?? "") };
-    },
-    async addAsFriend(_, args, context) {
-      if (!context.currentUser) {
-        throw new AuthenticationError("not authenticated");
-      }
-
-      const isFriend = (person: PersonType | null): boolean =>
-        !!context.currentUser?.friends
-          ?.map((friend: any) => friend?._id?.toString() ?? "")
-          .includes(person?._id.toString() ?? "");
-
-      const person = await Person.findOne({ name: args.name });
-      if (person && context.currentUser?.friends && !isFriend(person)) {
-        context.currentUser.friends =
-          context.currentUser?.friends?.concat(person);
-      }
-
-      await context.currentUser.save();
-      return context.currentUser;
-    },
-  },
-};
-
+const app = express();
+const httpServer = http.createServer(app);
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: "/",
+});
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const serverCleanup = useServer({ schema }, wsServer);
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  async context({ req }) {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.toLowerCase().startsWith("bearer ") && JWT_SECRET) {
-      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET) as
-        | { username: string; id: Types.ObjectId }
-        | undefined;
-      const currentUser = await User.findById(decodedToken?.id).populate(
-        "friends"
-      );
-      return { currentUser };
-    }
-    return undefined;
-  },
+  schema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
 });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+(async () => {
+  await server.start();
+  app.use(
+    "/",
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      // @ts-ignore
+      async context({ req }) {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.toLowerCase().startsWith("bearer ") && JWT_SECRET) {
+          const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET) as
+            | { username: string; id: Types.ObjectId }
+            | undefined;
+          const currentUser = await User.findById(decodedToken?.id).populate(
+            "friends"
+          );
+          return { currentUser };
+        }
+        return undefined;
+      },
+    })
+  );
+  const PORT = 4000;
+  httpServer.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}`);
+  });
+})();
