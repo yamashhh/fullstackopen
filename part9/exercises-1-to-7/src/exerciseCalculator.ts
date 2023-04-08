b2416169023a05a495b176a51b0dc0c66561b960
Shogo Yamato <yamash.yamash@gmail.com>
2023-04-08 19:58:09 +0900

    feat: finished exercise 9.2

---
+++ part9/exercises-1-to-7/src/exerciseCalculator.ts
@@ -0,0 +1,68 @@
+const RATING = {
+  HIGH: {
+    VALUE: 3,
+    DESCRIPTION: "Fantastic!",
+  },
+  NORMAL: {
+    VALUE: 2,
+    DESCRIPTION: "Great!",
+  },
+  LOW: {
+    VALUE: 1,
+    DESCRIPTION: "You can do it!",
+  },
+} as const;
+
+interface CalculateExercisesResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: (typeof RATING)[keyof typeof RATING]["VALUE"];
+  ratingDescription: (typeof RATING)[keyof typeof RATING]["DESCRIPTION"];
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  exerciseHours: number[],
+  target: number
+): CalculateExercisesResult => {
+  if (
+    exerciseHours.some((hours) => Number.isNaN(hours)) ||
+    Number.isNaN(target)
+  ) {
+    throw new Error("Invalid arguments.");
+  }
+
+  const periodLength = exerciseHours.length;
+  const trainingHours = exerciseHours.filter((hours) => hours > 0);
+  const trainingDays = trainingHours.length;
+  const average =
+    trainingHours.reduce(
+      (previousValue, currentValue) => previousValue + currentValue
+    ) / periodLength;
+  const success = average >= target;
+  const ratingThresholdBelow = target * 0.8;
+  const ratingThresholdAbove = target * 1.2;
+  const rating =
+    average > ratingThresholdAbove
+      ? RATING.HIGH.VALUE
+      : average > ratingThresholdBelow
+      ? RATING.NORMAL.VALUE
+      : RATING.LOW.VALUE;
+  const ratingDescription =
+    Object.values(RATING).find((element) => element.VALUE === rating)
+      ?.DESCRIPTION ?? RATING.LOW.DESCRIPTION;
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
