30b5030647aff135dc277476a8c78c2d2c5b5c79
Shogo Yamato <yamash.yamash@gmail.com>
2023-05-04 19:35:49 +0900

    feat: finished exercises -10.2

---
+++ part10/rate-repository-app/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    "universe/native",
+    "universe/shared/typescript-analysis",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "standard-with-typescript",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
