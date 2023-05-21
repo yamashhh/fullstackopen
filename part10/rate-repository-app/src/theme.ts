de476a808eba34a7adaf5f25836a69124d1c8a2c
Shogo Yamato <yamash.yamash@gmail.com>
2023-05-21 22:18:59 +0900

    feat: finished exercise 10.10

--- part10/rate-repository-app/src/theme.ts
+++ part10/rate-repository-app/src/theme.ts
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
@@ -13,7 +15,11 @@ const theme = {
     heading: 18,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
